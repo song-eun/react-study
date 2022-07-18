3c53c96187990695cb318cbae375132c897e1915
song-eun <dmswo546@naver.com>
2022-07-18 20:29:33 +0900

    addNewMeetup update

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
-function NewMeetupPage(){
-    return <div>NewMeetupPage Page</div>
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
